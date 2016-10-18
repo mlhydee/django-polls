aad597c6de25ad948119cacf0992fe4727c00497
Mary <mary.hyde@dootrix.com>
2016-10-18 10:16:17 +0100

    Changed return self.question_text to choice_text in class Choice

--- polls/models.py
+++ polls/models.py
@@ -17,7 +17,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
@@ -25,4 +26,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default = 0)
 
     def __str__(self):
-        return self.question_text
+        return self.choice_text
